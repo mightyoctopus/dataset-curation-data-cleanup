65ecaa5a2c6582ca4255c442fde3575d5d0c9270
mhhong <mh.hong@proton.me>
2025-10-24 18:13:47 +0900

    added a new class for parsing dataset

---
+++ items.py
@@ -0,0 +1,135 @@
+from typing import Optional
+from unicodedata import category
+
+from transformers import AutoTokenizer
+import re
+
+BASE_MODEL = "meta-llama/Llama-3.1-8B"
+MIN_TOKENS = 150
+MAX_TOKENS = 160
+MIN_CHARS = 300
+CEILING_CHARS = MAX_TOKENS * 7
+
+
+class Item:
+    """
+    An Item is a cleaned, curated datapoint of a Product with a Price.
+    """
+
+    tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL, trust_remote_code=True)
+    PREFIX = "Price is $"
+    QUESTION = "How much does this cost to the nearest dollar?"
+    REMOVALS = [
+        "Manufacturer",
+        "Language",
+        "Best Sellers Rank",
+        "Is Discontinued By Manufacturer"
+    ]
+
+    title: str
+    price: float
+    category: Optional[str]
+    token_count: int
+    details: Optional[str]
+    prompt: Optional[str]
+    include = False
+
+    def __init__(self, data, price):
+        self.title = data["title"]
+        self.price = data["price"]
+        self.token_count = 0
+        self.include = False
+        self.prompt = None
+        self.details = None
+        self.category = None
+        self.parse(data)
+
+
+    def scrub_details(self):
+        """
+        Clean up the details string by removing unnecessary text that doesn't add value.
+        """
+        details = self.details
+        for remove in Item.REMOVALS:
+            details = details.replace(remove, "")
+
+        return details
+
+    def scrub(self, stuff):
+        """
+        Clean up the provided text by removing unnecessary characters and whitespace
+        Also, remove words that are 7+ chars and contain numbers as there are likely
+        irrelevant product numbers.
+        """
+        stuff = re.sub(r"[:\[\]{}[] \s]", " ", stuff).strip()
+        stuff = stuff.replace(" ,", ",").replace(",,,", ",").replace(",,", ",")
+        words = stuff.split(" ")
+        select = [word for word in words if len(word) < 7 or not any(char.isdigit() for char in word)]
+        return " ".join(select)
+
+    def parse(self, data):
+        """
+        Parse this datapoint and if it fits within the allowed Token range
+        then set include to "True"
+        """
+        contents = "\n".join(data["description"])
+        if contents:
+            contents += "\n"
+
+        features = "\n".join(data["features"])
+        if features:
+            contents += features + "\n"
+
+        self.details = data["details"]
+        if self.details:
+            contents += self.scrub_details() + "\n"
+
+        if len(contents) > MIN_CHARS:
+            contents = contents[:CEILING_CHARS]
+            text = f"{self.scrub(self.title)}\n{self.scrub(contents)}"
+            tokens = Item.tokenizer.encode(text, add_special_tokens=False)
+            if len(tokens) > MIN_TOKENS:
+                tokens = tokens[:MAX_TOKENS]
+                self.make_prompt(text)
+                self.include = True
+
+    def make_prompt(self, text):
+        """
+        Set the prompt instance variable to be a prompt appropriate for training
+        """
+        self.prompt = f"{self.QUESTION}\n\n{text}\n\n"
+        self.prompt += f"{self.PREFIX}{str(round(self.price))}.00"
+        self.token_count = len(Item.tokenizer.encode(self.prompt, add_special_tokens=False))
+
+    def test_prompt(self):
+        """
+        Return a prompt suitable for testing, with the actual price(answer) removed
+        so that the LLM model response can be evaluated.
+        """
+        return self.prompt.split(self.PREFIX)[0] + self.PREFIX
+
+    def __repr__(self):
+        """
+        Return a string version of the Item class.
+        """
+        return f"<{self.title} = ${self.price}>"
+
+
+
+###===============================================================================###
+### Test ###
+###===============================================================================###
+
+amazon_data = {
+    "title": "Dell Laptop 14 inches",
+    "price": 1400,
+    "description": "this is an awesome laptop!!",
+    "features": "wireless internet connection",
+    "details": "Great one."
+}
+
+# print(amazon_data["title"])
+
+item = Item(amazon_data, amazon_data)
+
+print(item.make_prompt())
